fb6c81aa2d1f07984eae49d2d5c8d053cd5212bc
parsashop24 <168118919+parsashop24@users.noreply.github.com>
2024-04-30 01:39:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+document.addEventListener('contextmenu', event => event.preventDefault());
+
+document.onkeypress = function (event) {
+event = (event || window.event);
+if (event.keyCode == 123) {
+return false;
+}
+}
+document.onmousedown = function (event) {
+event = (event || window.event);
+if (event.keyCode == 123) {
+return false;
+}
+}
+document.onkeydown = function (event) {
+event = (event || window.event);
+if (event.keyCode == 123) {
+return false;
+}
+}
+
 document.addEventListener("DOMContentLoaded", function() {
     var faqItems = document.querySelectorAll('.faq-item');
     faqItems.forEach(function(item) {
