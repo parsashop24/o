4133de9c6c7ea7f95f8d3091dc80f0e2e2837d3b
parsashop24 <168118919+parsashop24@users.noreply.github.com>
2024-08-25 17:57:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -54,5 +54,5 @@ var formattedTomanPrice = tomanPrice.toLocaleString();
     
     document.getElementById("result").innerHTML = "قیمت نهایی تحویل در ایران (به یورو): " + "<strong>" + totalPrice.toFixed(2) + "</strong>";
 
-    document.getElementById("additionalInfo").innerHTML = "قیمت نهایی تحویل در ایران (به تومان): " + "<strong>" + formattedTomanPrice  + "</strong>";
+  
 }
